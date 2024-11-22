274b7d1e646215074e2bafc0fc672cd32827896d
amanda-bann <amanda_klap@protonmail.com>
2024-11-22 12:11:48 -0700

    Create global variables

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const textInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+// Initial word to test the game
+const word = "magnolia";
+
